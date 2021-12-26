3f6809b7ae8af49ee1c3b9916663563c19fb5103
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-26 19:58:30 -0300

    construindo component input

---
+++ src/components/Input.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../login.css';
+
+export default function Input({name, onclick, onChange, type, placeholder, label, className}){
+    return(
+        <>
+          <input
+            onChange={onChange}
+            onclick={onclick}
+            type={type}
+            placeholder={placeholder}
+            name={name}
+            className={className}
+          >
+            {label}
+          </input>
+        </>
+    );
+}
