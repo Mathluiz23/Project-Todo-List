abd8b3241c8c481aab24d95fe1c19dea2dc9758d
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-26 19:59:11 -0300

    construindo component Todo

---
+++ src/components/Todo.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import { MdDeleteForever } from 'react-icons/md';
+import { TiEdit } from 'react-icons/ti';
+import '../App.css';
+
+function Todo({ todos, doneTask, removeTask, updateTask,  }) {
+  const [editTask, setEditTask] = useState({ value: ''});
+  
+  function submitUpdate(value) {
+    updateTask(editTask.id, value);
+    setEditTask({
+      value: ''
+    });
+  };
+
+  if (editTask.id) {
+    return <TodoForm edit={editTask} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => doneTask(todo.id)}>
+        {todo.text}
+      </div>
+      <div className='icons'>
+        <MdDeleteForever
+          onClick={() => removeTask(todo.id)}
+          className='delete-icon'
+        />
+        <TiEdit
+          onClick={() => setEditTask({ id: todo.id, value: todo.text })}
+          className='edit-icon'
+        />
+      </div>
+    </div>
+  ));
+};
+
+export default Todo;
